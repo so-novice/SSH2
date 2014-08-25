38065cd77915f3b55fa41b5738926137d7eeb4ae
jiangq <novice.js@gmail.com>
2014-08-25 12:48:17 +0800

    提交测试

--- ssh/src/com/seed/testAction/login.java
+++ ssh/src/com/seed/testAction/login.java
@@ -8,6 +8,8 @@ import com.opensymphony.xwork2.ActionSupport;
 public class login extends ActionSupport{
     @Override
     public String execute() throws Exception {
+
         return super.execute();
+
     }
 }
